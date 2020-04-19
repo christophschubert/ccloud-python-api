077e58f60a05b5df6cfbda700a3fb05dcaa10ec6
Christoph Schubert <christoph.schubert@tu-dortmund.de>
2020-04-19 18:35:04 +0200

    Added --prefix and --quiet options

--- create-producer.py
+++ create-producer.py
@@ -8,6 +8,8 @@ parser.add_argument('--kafka-cluster-id', dest='kafka', metavar='lkc-xxx', requi
 parser.add_argument('--topic', dest='topic', required=True, help='Topic name')
 parser.add_argument('--sa-name', dest='sa_name', required=True, help='Name of service-account')
 parser.add_argument('--sa-description', dest='sa_description', help='description of service account')
+parser.add_argument('--prefix', action='store_true')
+parser.add_argument('--quiet', action='store_true')
 
 args = parser.parse_args()
 
@@ -18,13 +20,13 @@ if args.sa_description:
     description = args.sa_description
 else:
     description = 'SA for write access to topic '.format(topic)
-is_prefix = False
+is_prefix = args.prefix
 
 kafka_sa = ccloud.create_service_account(sa_name, description)
 kafka_api_key = ccloud.create_api_key(kafka_cluster, kafka_sa['id'], 'key for ' + description)
 ccloud.create_acl_entry('allow', 'write', kafka_sa['id'], 'topic', topic, is_prefix)
-
-print('created service account {} with rights to produce to {} using key {}'.format(sa_name, topic, kafka_api_key['key']))
-print('copy the following lines to a properties file\n')
+if not args.quiet:
+    print('created service account {} with rights to produce to {} using key {}'.format(sa_name, topic, kafka_api_key['key']))
+    print('copy the following lines to a properties file\n')
 
 print(ccloud.create_jaas(kafka_api_key))
